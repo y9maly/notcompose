67d1c8d3da9e40db215d39366fc3080b1540bea9
y9maly <y9maly@gmail.com>
2026-05-23 15:30:19 +0300

    refactor: Key creates a recompose scope now

--- src/notcompose/runtime-highlevel/Key.ts
+++ src/notcompose/runtime-highlevel/Key.ts
@@ -1,9 +1,11 @@
 import {currentComposer} from "../runtime/currentComposer.js";
 import {Modifier} from "../runtime/Modifier.js";
 import {Key} from "../runtime/Composer.js";
+import {RecomposeLambdaExtensionKey} from "../runtime-plugins/partialRecomposition/RecomposeLambda";
 
 export function Key<T>(key: Key, content: () => T): T {
     currentComposer().startNode(new Modifier(), key)
+    currentComposer().applyExtension(RecomposeLambdaExtensionKey, content)
     currentComposer().startComposingNode()
     const value = content()
     currentComposer().endComposingNode()
