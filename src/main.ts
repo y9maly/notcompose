70f9bb7b31acf5d265f537543533c13f580565c4
y9maly <y9maly@gmail.com>
2026-06-14 17:40:18 +0300

    docs(examples): add example scripts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import {Modifier} from "notcompose";
-import {OffsetModifier} from "notcompose/layout";
-import {setTerminalContent, Text} from "notcompose/terminal";
+import {Column, OffsetModifier} from "notcompose/layout";
+import {Color, colored, setTerminalContent, Text} from "notcompose/terminal";
 
 setTerminalContent(() => {
-
+    Column(() => {
+        Text("Try to write your code here!")
+        Text(colored(Color.Red, './src/main.ts'))
+    }, new Modifier([OffsetModifier(2, 1)]))
 })
